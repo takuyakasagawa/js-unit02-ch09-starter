3cf1c474b31cbd7bf14d6c7ed0b6b38dcfc35e5f
takuyakasagawa <kasa.kasa.0062362@gmail.com>
2020-12-01 21:07:36 +0900

    edited JS

---
+++ src/lib/MailValidator.test.js
@@ -0,0 +1,7 @@
+import MailValidator from './MailValidator';
+
+describe('Mail validator', () => {
+  it('妥当な場合は成功を返す', () => {});
+  it('空の場合は失敗を返す', () => {});
+  it('フォーマットが異なる場合は失敗を返す', () => {});
+});
