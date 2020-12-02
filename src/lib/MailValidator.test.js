00ff4d053eb9db8a5c474d25c070ce9a0dbf6d05
takuyakasagawa <kasa.kasa.0062362@gmail.com>
2020-12-02 19:24:11 +0900

    edited js

--- src/lib/MailValidator.test.js
+++ src/lib/MailValidator.test.js
@@ -5,3 +5,36 @@ describe('Mail validator', () => {
   it('空の場合は失敗を返す', () => {});
   it('フォーマットが異なる場合は失敗を返す', () => {});
 });
+
+describe('password validator', () => {
+  it("妥当な場合は成功を返す", () => {
+    const Mail = "samplePass1";
+    const validator = new MailValidator(Mail);
+    return validator.validate()
+      .then((res) => {
+        expect(res).toEqual({
+          success: true
+        });
+    });
+  });
+
+  it("空の場合は失敗を返す", () => {
+    const Mail = "";
+    const validator = new MailValidator(Mail);
+    return validator.validate()
+      .then((res) => {
+        expect(res.success).toBeFalsy();
+        expect(res.message).toBe('メールアドレスは必須です。')
+      });
+  })
+
+  it("フォーマットが異なる場合は失敗を返す", () => {
+    const Mail = "test";
+    const validator = new MailValidator(Mail);
+    return validator.validate()
+      .then((res) => {
+        expect(res.success).toBeFalsy();
+        expect(res.message).toBe('フォーマットが異なります。')
+      });
+  })
+});
